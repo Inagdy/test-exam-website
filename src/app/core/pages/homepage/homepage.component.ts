99817f8c99a10a248eb9285e37721291e0b6f592
Inagdy <islam.m.nagdy@gmail.com>
2024-12-14 12:23:07 +0200

    adding home page and font awsome

--- src/app/core/pages/homepage/homepage.component.ts
+++ src/app/core/pages/homepage/homepage.component.ts
@@ -1,12 +1,18 @@
+
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
-export class HomepageComponent {
+export class HomepageComponent  {
+ 
 
 }
+
